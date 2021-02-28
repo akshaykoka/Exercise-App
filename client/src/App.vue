048b4344d6425c7f1ec103f5c311501f2cd99eb0
akshay <kokaa2@hawkmail.newpaltz.edu>
2021-02-27 22:29:00 -0400

    Creating vue client from vue starter project

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+   <div id="app">
+        <div id="nav">
+          <!-- including home page on the APP module-->
+          <router-view/>
+          <router-link to="/"></router-link> |
+          <Mynav></Mynav>
+        </div>
+   </div>
+</template>
+
+//Importing and exporting the feature so that we can use it in APP module
+<script>
+   import Mynav from "../src/components/Nav";
+   export default{
+        components:{
+                  Mynav
+      }
+   }
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+   <div>
+     <nav class="level">
+       <div class="level-left">
+          <p class="level-item title is-2 has-text-centered">
+              <a class="link is-info is-large">My Fitness Tracker</a>
+          </p>
+        </div>
+        <div class="level-right subtitle is-bold right-content">
+           <p class="level-item ">
+              <a class="link is-link "><router-link to="/SignUp">Sign Up</router-link></a>
+           </p>
+           <p class="level-item ">
+             <a class="link is-link "><router-link to="/Login">Log In</router-link></a>
+          </p>
+        </div>
+    </nav>
+  </div>
+</template>
+//Exporting the page so that other vue modules can access it
+<script>
+// @ is an alias
+export default {
+  name: 'Home'
+}
+</script>
+<style scoped>
+    .right-content{
+        margin-right: 20px;
+    }
+</style>
