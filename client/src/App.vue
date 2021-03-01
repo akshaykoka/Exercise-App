652ecb61ea1aaf866452cc6268c3b82d57db8456
akshay <kokaa2@hawkmail.newpaltz.edu>
2021-03-01 18:10:22 -0400

    node modules and version change

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,21 @@
 <template>
-   <div id="app">
-        <div id="nav">
-          <!-- including home page on the APP module-->
-          <router-view/>
-          <router-link to="/"></router-link> |
-          <Mynav></Mynav>
-        </div>
-   </div>
+  <h1>Hello world</h1>
 </template>
-
-//Importing and exporting the feature so that we can use it in APP module
 <script>
-   import Mynav from "../src/components/Nav";
-   export default{
-        components:{
-                  Mynav
-      }
-   }
+export default {
+  name: 'App',
+  components: {
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,27 +1,12 @@
-//Importing vue module
 import Vue from 'vue'
-//Importing App view,so that application will start from APP page
 import App from './App.vue'
-//Importing router module so that we can decalre path routing
 import router from './router'
-//Importing Bulma css
-import "bulma"
-//Importing font awesome for Icons
-import '@fortawesome/fontawesome-free/css/all.css'
-import store from '../src/router/store.js'
-import ToggleButton from 'vue-js-toggle-button'
-Vue.use(ToggleButton);
-//For auto complete feature
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
 
-Vue.use(Buefy)
-import vSelect from 'vselect-component'
-Vue.use(vSelect)
-//Creating Vue Instance
-Vue.config.productionTip = false;
+
+
+Vue.config.productionTip = false
+
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
